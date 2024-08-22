61ef59e193daf39c7a33ae5868557b42022d47e7
AyaNS2003 <aya.sawalha.2003@gmail.com>
2024-08-22 11:48:47 +0300

    decrease duplication in product and row material class

--- src/main/java/main_entities/Product.java
+++ src/main/java/main_entities/Product.java
@@ -22,76 +22,44 @@ public class Product {
     }
 
     public Product(String productName, int price, int wholesalePrice, int quantity, int saledQty, String exDate, String ownerEmail) {
-        this.productName = productName;
-        this.price = price;
-        this.wholesalePrice = wholesalePrice;
-        this.quantity = quantity;
-        this.saledQty = saledQty;
-        this.exDate = exDate;
-        this.ownerEmail = ownerEmail;
+        this(0,productName,price,wholesalePrice, quantity,saledQty,exDate,ownerEmail);
     }
 
     public int getProductId() {
         return productId;
     }
 
-    public void setProductId(int productId) {
-        this.productId = productId;
-    }
 
     public String getProductName() {
         return productName;
     }
 
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
 
     public int getPrice() {
         return price;
     }
 
-    public void setPrice(int price) {
-        this.price = price;
-    }
 
     public int getWholesalePrice() {
         return wholesalePrice;
     }
 
-    public void setWholesalePrice(int wholesalePrice) {
-        this.wholesalePrice = wholesalePrice;
-    }
 
     public int getQuantity() {
         return quantity;
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
 
     public int getSaledQty() {
         return saledQty;
-    }
-
-    public void setSaledQty(int saledQty) {
-        this.saledQty = saledQty;
     }
 
     public String getExDate() {
         return exDate;
     }
 
-    public void setExDate(String exDate) {
-        this.exDate = exDate;
-    }
-
     public String getOwnerEmail() {
         return ownerEmail;
     }
 
-    public void setOwnerEmail(String ownerEmail) {
-        this.ownerEmail = ownerEmail;
-    }
 }
--- src/main/java/main_entities/RawMaterial.java
+++ src/main/java/main_entities/RawMaterial.java
@@ -10,15 +10,6 @@ public class RawMaterial {
     private String exDate;
     private String supplierEmail;
 
-    public RawMaterial(String rmName, int price, int wholesalePrice, int quantity, int saledQty, String exDate, String supplierEmail) {
-        this.rmName = rmName;
-        this.price = price;
-        this.wholesalePrice = wholesalePrice;
-        this.quantity = quantity;
-        this.saledQty = saledQty;
-        this.exDate = exDate;
-        this.supplierEmail = supplierEmail;
-    }
     public RawMaterial(int rmId, String rmName, int price, int wholesalePrice, int quantity, int saledQty, String exDate, String supplierEmail) {
         this.rmId = rmId;
         this.rmName = rmName;
@@ -29,70 +20,50 @@ public class RawMaterial {
         this.exDate = exDate;
         this.supplierEmail = supplierEmail;
     }
+    public RawMaterial(String rmName, int price, int wholesalePrice, int quantity, int saledQty, String exDate, String supplierEmail) {
+        this(0,rmName,price,wholesalePrice,quantity,saledQty,exDate,supplierEmail);
+    }
+
 
     public int getRmId() {
         return rmId;
     }
 
-    public void setRmId(int rmId) {
-        this.rmId = rmId;
-    }
 
     public String getRmName() {
         return rmName;
     }
 
-    public void setRmName(String rmName) {
-        this.rmName = rmName;
-    }
 
     public int getPrice() {
         return price;
     }
 
-    public void setPrice(int price) {
-        this.price = price;
-    }
 
     public int getWholesalePrice() {
         return wholesalePrice;
     }
 
-    public void setWholesalePrice(int wholesalePrice) {
-        this.wholesalePrice = wholesalePrice;
-    }
 
     public int getQuantity() {
         return quantity;
     }
 
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
 
     public int getSaledQty() {
         return saledQty;
     }
 
-    public void setSaledQty(int saledQty) {
-        this.saledQty = saledQty;
-    }
 
     public String getExDate() {
         return exDate;
     }
 
-    public void setExDate(String exDate) {
-        this.exDate = exDate;
-    }
 
     public String getSupplierEmail() {
         return supplierEmail;
     }
 
-    public void setSupplierEmail(String supplierEmail) {
-        this.supplierEmail = supplierEmail;
-    }
 
     @Override
     public String toString() {
